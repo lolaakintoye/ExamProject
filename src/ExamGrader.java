c12f0fe8bc6e4f613e80bcb181cd80e0fa95cc0d
Daisy Arellano <darell3@uic.edu>
2018-04-11 11:32:58 -0500

    Update ExamGrader.java

--- src/ExamGrader.java
+++ src/ExamGrader.java
@@ -1,212 +1,169 @@
-//* CS 342: Project 4: Developing Applications Using Exam-Related Classes
- * Name: Daisy Arellano
- * NetID: darell3
+/* CS 342: Project 3: Saving and Restoring Exam and Exam Components
+ * Name: Charlotte Norman
+ * NetID: cnorma4
  */
 
 import java.io.*;
 import java.util.*;
-import java.text.*;
-//import java.time.*;
 
-public class Exam
+import java.io.FileWriter;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class ExamGrader 
 {
-	private String title; // Title of the exam.
-	protected ArrayList<Question> questions; // Array list to hold Question objects (exam questions).
-    private int numQuestions; // Total number of questions in an exam.    
-	private double studentScore; // Actual Student Score 
-	private double maxScore; // Total Possible Score
-	private double examScore; // Exam Score
-	
-	private String[] questionTypes = new String[] {"mcsaquestion", "mcmaquestion", "saquestion", "numquestion"}; // Array to hold question types.
-	private String fileLine; // The lines of the exam file.
+	private static String studentName;
+	private static String examFileName;
+	private static String ansFileName;
+	public static Scanner getExamFileScanner() { // Gets the file used to create an exam.
+		Scanner scanUserInput = ScannerFactory.getKeyboardScanner(); // For user input.
+		Scanner scanFile; // Reads the exam file that will contain the exam contents.
+		//String examFileName; // The filename of the exam file.
+		File examFile; // The actual file object of the exam.
 		
-	public Exam(String title) {
-		this.title = title;
-		questions = new ArrayList<Question>();
-		numQuestions = 0;
-		studentScore = 0.0;
-		maxScore = 0.0;
-		examScore = 0.0;
-	}
-	
-	public Exam(Scanner scn) {
-		questions = new ArrayList<Question>();
-		numQuestions = 0;
-		studentScore = 0.0;
-		maxScore = 0.0;
-		examScore = 0.0;
-
-		// Reading in Exam Information
+		// Getting User Input for the Exam File
 		
-		title = scn.nextLine(); // Gets exam title from scanner.
+		System.out.print("Enter an exam filename: ");
+		examFileName = scanUserInput.nextLine();
 		
-		while (scn.hasNextLine()) { // Reads until EOF.			
-			fileLine = scn.nextLine();
+		// Processing Exam File 
 			
-			while (fileLine.length() == 0) { // If the current line is empty (length 0) then skip to the next until it is not.
-				fileLine = scn.nextLine();
-			}
-			
-			for (int i = 0; i < questionTypes.length; i++) {
-				if ((fileLine.toLowerCase().equals(questionTypes[i]))) { // If the line equals one of the question types, then create the question.			
-					switch(fileLine.toLowerCase()) {
-						case "mcsaquestion": // Creates MCSAQuestion object.
-							this.addQuestion(new MCSAQuestion(scn)); // Adds question to exam.
-							break;
-						case "mcmaquestion": // Creates MCMAQuestion object.
-							this.addQuestion(new MCMAQuestion(scn)); // Adds question to exam.
-							break;
-						case "saquestion": // Creates SAQuestion object.
-							this.addQuestion(new SAQuestion(scn)); // Adds question to exam.
-							break;
-						case "numquestion": // Creates NumQuestion object.
-							this.addQuestion(new NumQuestion(scn)); // Adds question to exam.
-							break;
-						default:
-							break;				
-					}
-				}
-			}				
-		}
-			
-		System.out.println("\n\n" + numQuestions + " questions has been added to the exam.\n");	
-	}
-	
-	public void print() {	
-		int i = 0;
-		
-		System.out.println("\nExam: " + title + "\n"); // Prints exam title.
-		
-		for (Question q: questions) { // Prints Questions
-			System.out.print("Question " + (i + 1) + ". ");
-			q.print(); 
-			i++; // To get the questions' positions in their current order.
-		}
-	}
-	
-	public void addQuestion(Question quesObj) {
-		questions.add(quesObj);
-		numQuestions++;				
-	}
-	
-	public void reorderQuestions() { // Reordering all the questions.	
-		Collections.shuffle(questions); 
-	}
-	
-	public void reorderMCQuestions(int position) {
-		if (position < 0) { // Reordering all the multiple choice questions' answers.
-		
-			for (Question q: questions) { 
-				if (q instanceof MCQuestion) { // Checking if the question is a multiple choice type.
-					((MCQuestion) q).reorderAnswers();
-				}
+		while (true) { // Check if the file actually exists.  If not, get a new file input.
+			try {
+				examFile = new File(examFileName); // Opens specified file.
+				scanFile = new Scanner(examFile); 
+				break;
+				
+			} catch(Exception e) { // Likely a "file not found" exception, but catches any just in case.
+				System.out.println("File: " + examFileName);
+				System.out.print("Error: That file does not exist.  Please enter a new filename: ");
+				examFileName = scanUserInput.nextLine();
 			}
 		}
-		else if (position < numQuestions) { // Makes sure the position is in bounds.
-											// Reordering just the applicable multiple choice question's answers.
-			Question q = questions.get(position);
-			
-			if (q instanceof MCQuestion) {
-				((MCQuestion) q).reorderAnswers(); // Checking if the question is a multiple choice type.
-			}
-		}
+				
+		return scanFile;
 	}
 	
-	public void getAnswerFromStudent(int position) {		
-		if (position < 0) { // Gets an answer for every question in the exam.	
-			for (Question q: questions) { 
-				q.getAnswerFromStudent();
-			}
-		}
-		else if (position < numQuestions) { // Makes sure the position is in bounds.
-											// Just gets an answer for a particular question.
-			Question q = questions.get(position);
-			q.getAnswerFromStudent();
-		}		
-	}
-	
-	public double getValue() {		
-		for (Question q: questions) { // Goes through each question to get the two scores to sum up separately.
-			studentScore += q.getValue();
-			maxScore += q.getMaxValue();
-		}
+	public static Scanner getAnsFileScanner(String ansFileName) { // Gets the file used to create an Answer File.
+		Scanner savedAnswersScanner = ScannerFactory.getKeyboardScanner(); // For user input.
+		//String ansFileName; // The filename of the exam file.
+		File ansFile; // The actual file object of the exam.
 		
-		if (studentScore == 0.0 && maxScore == 0.0) { // Error checking: in case there are no questions on the exam to grade.
-			examScore = -1.0;
+		// Getting User Input for the Exam File
+		
+		// Processing Exam File 
+		while (true) { // Check if the file actually exists.  If not, get a new file input.
+			try {	
+				ansFile = new File(ansFileName); // Opens specified file.
+				savedAnswersScanner = new Scanner(ansFile); // Scanner to read student answers.
+				break;
+				
+			} catch(Exception e) { // Likely a "file not found" exception, but catches any just in case.
+				System.out.println("File: " + ansFileName);
+				System.out.print("Error: That file does not exist.  Please enter a new filename: ");
+				ansFileName = savedAnswersScanner.nextLine();
+			}
+		}
+				
+		return savedAnswersScanner;
+	}
+	
+	
+	public static void main(String args[]) {
+		Scanner scn = ScannerFactory.getKeyboardScanner(); // For user input.
+		Scanner examFileScanner; // Scanner containing the exam contents.
+		Scanner examScanner;
+		String csvName;
+		Scanner csvScanner;
+		String AnswerTitle;
+		PrintWriter csvWriter;
+		Exam currExam; // Current Exam Object
+		double examScore; // Final Exam Score		
+		
+		// Printing Program Introduction
+		
+		System.out.println("Starting up...");
+		System.out.println("Student Name: Daisy Arellano");
+		System.out.println("Student NetID: darell3 \n");
+			
+		// Creating and Executing the Exam
+		
+		examFileScanner = getExamFileScanner();
+		
+		currExam = new Exam(examFileScanner); // Step 1	
+		System.out.println("\nPlease enter the filename to save the student answers under: ");
+		ansFileName = scn.nextLine();
+		scn = getAnsFileScanner(ansFileName);
+		//String FileLine;
+				
+		//System.out.println(scn.nextLine());
+		studentName = scn.nextLine();
+		//Title of the answer file
+		AnswerTitle = scn.nextLine();
+		//save exam file name
+		String temp = examFileName.toLowerCase();
+		//save answer title
+		AnswerTitle = AnswerTitle.toLowerCase();
+		// check if files match
+		if(AnswerTitle.equals(temp)) {
+			System.out.println("Files Match");
 		}
 		else {
-			examScore = studentScore;
+			System.out.println("Mismatched file please run program again with matching File.");
+		}
+		scn.close();
+
+		scn = getAnsFileScanner(ansFileName);
+		//System.out.println();
+
+		currExam.restoreStudentAnswers(scn); 
+		
+	    //currExam.print();
+		
+		examScore = currExam.getValue();
+		
+		if (examScore == -1.0) {
+			System.out.println("There are no questions or answers to grade!");		
+		}
+		else {
+			currExam.reportQuestionValues(); 		
+			System.out.println("\n\nThe exam is finished! Your score is " + examScore + ".");
 		}
 		
-		return examScore;
-	}
-	
-	public void reportQuestionValues() {
-		int i = 0;
+		//scn.close();
 		
-		System.out.println("\n\n              +---- " + title + " Exam Score ----+                 ");
-		System.out.println("+----            Question  | Question Score         ----+");
+		System.out.println("Enter file where results should be stored: ");
 		
-		for (Question q: questions) { // Goes through each question to print out relevant scoring information.
-			System.out.println("+----           Question " + (i + 1) + " | " + new DecimalFormat("#.##").format(q.getValue()) + "                    ----+"); // Keeps it to two decimal places.
-			i++;
+		scn = ScannerFactory.getKeyboardScanner(); // For user input.
+		
+		csvName = scn.nextLine();
+		
+		while (true) { // Check if the file actually exists.  If not, get a new file input.
+			try {	
+				//File csvFile = new File(csvName); // Opens specified file.
+				csvWriter = new PrintWriter (csvName);
+				break;
+				
+			} catch(Exception e) { // Likely a "file not found" exception, but catches any just in case.
+				System.out.println("File: " + ansFileName);
+				System.out.print("Error: That file does not exist.  Please enter a new filename: ");
+			}
 		}
-		
-		System.out.println("+----      Your Score: " + examScore + " | Total Score: " + maxScore + "      ----+");
-	}
-	
-	public void save(PrintWriter pw) {
-		Date currentDate = new Date();
-		
-		pw.println(title);
-		pw.println(currentDate.toString());
-		
-		for (Question q: questions) {
-			pw.println("");
-			q.save(pw);		
+		csvWriter.print("Students Name, Total Score, ");
+		for(int i = 0; i < currExam.numQuestions(); i++) {
+			csvWriter.print("Question " + (i+1) + ",");
 		}
-	}
-	
-	public void saveStudentAnswers(PrintWriter pw) {
-		Date currentDate = new Date();
+		csvWriter.println();
+		csvWriter.print(studentName + ", " + examScore);
+		currExam.saveQuestionValue(csvWriter);
+		csvWriter.close ();  
+		examFileScanner.close();
+		ScannerFactory.closeKeyboardScanner();
 		
-		Scanner scn = ScannerFactory.getKeyboardScanner(); // The shared System.in scanner.
-		pw.println(currentDate.toString());
-		
-		System.out.print("\nWhat is your name? ");
-		pw.println(scn.nextLine());
-		pw.println(title);
-		
-		for (Question q: questions) {
-			pw.println("");
-			q.saveStudentAnswer(pw);		
-		}
-	}
-	
-	public void restoreStudentAnswers(Scanner scn) {
-		scn.nextLine(); // Gets the student name and discards.
-		
-		scn.nextLine(); // scan title and discards
-		
-		//scn.nextLine(); // scan title and discards
-		
-		for (Question q: questions) {
-			//System.out.println(q);
-			scn.nextLine();
-			q.restoreStudentAnswer(scn);
-		}
-	}
-	
-	public int numQuestions() {
-		return numQuestions;
-	}
-	
-	public void saveQuestionValue(PrintWriter pw) {
-		int i = 0;
-		
-		for (i = 0; i < questions.size(); i++) {
-			pw.print("," + questions.get(i).getValue() );
-		}
-		//pw.print(questions.get(questions.size()).getValue());
+		return;
 	}
 }
